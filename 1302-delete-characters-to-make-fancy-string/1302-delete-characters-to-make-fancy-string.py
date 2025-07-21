6cfe261326c6c73d9f5b9231484be0b2b45af4c1
Muhammad Haider Ijaz <2022ag7999@uaf.edu.pk>
2025-07-21 23:09:45 +0500

    Time: 301 ms (75.75%), Space: 19.7 MB (52.29%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -1,18 +1,10 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        result = s[0]
-        last = s[0]
-        count = 1
+        result = []
 
-        for i in range(1, len(s)):
-            if s[i] != last:
-                last = s[i]
-                count = 0
-
-            count += 1
-            if count > 2:
-                continue
-
-            result += s[i]
-
-        return result
+        for char in s:  
+            if len(result) >= 2 and result[-1] == char and result[-2] == char:
+                continue 
+            result.append(char) 
+        
+        return ''.join(result) 
