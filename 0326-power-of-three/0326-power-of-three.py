0a3e029ae93d0e592e65db476e3f16f64462a608
Muhammad Haider Ijaz <2022ag7999@uaf.edu.pk>
2025-08-14 00:43:16 +0500

    Time: 3 ms (92.63%), Space: 17.9 MB (25.7%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -2,8 +2,6 @@ class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         if n<=0 :
             return False
-        elif n==1:
-            return True
         while n%3==0:
             n//=3
         return n==1
